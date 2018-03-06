a9e178405eb08538f01f47afdac040d1998a95d0
diop <fodesdiop@gmail.com>
2018-03-06 11:12:08 -0800

    Add exchange contract scaffolding

--- contracts/JotaliExchange.sol
+++ contracts/JotaliExchange.sol
@@ -1,7 +1,93 @@
 pragma solidity ^0.4.19;
 
-contract JotaliExchange {
-  function JotaliExchange {
+import "./owned.sol";
+import "./JotaliToken.sol";
 
-  }
+contract JotaliExchange is owned{
+
+    function JotaliExchange() {
+
+    }
+
+    // General Structure
+    struct Offer {
+
+    }
+
+    struct OrderBook {
+
+    }
+
+    struct Token {
+
+    }
+
+    //Support a max of 255 tokens.
+    mapping (uint8 => Token) tokens;
+    uint symbolNameIndex;
+
+    // Balances
+
+    mapping (address => mapping (uint8 => uint)) tokenBalanceForAddress;
+
+    mapping (address => uint) balanceEthForAddress;
+
+    // Ether Deposit and Withdrawal
+
+    function depositEther() payable {
+
+    }
+
+    function withdrawEther(uint amountInWei) {
+
+    }
+
+    function getEtherBalanceInWei() constant returns (uint) {
+
+    }
+
+    // Token Management
+
+    function addToken(string symbolName, address erc20TokenAddress) onlyOwner {
+
+    }
+
+    function hasToken(string symbolName) constant returns (bool) {
+
+    }
+
+    function getSymbolIndex(string symbolName) internal returns (uint8) {
+
+    }
+
+    // Order Book - Bid Orders
+
+    function getBuyOrderBook(string symbolName) constant returns (uint[], uint[]) {
+
+    }
+
+    // Order Book - Ask Orders
+
+    function getSellOrderBook(string symbolName) constant returns (uint[], uint[]) {
+
+    }
+
+    // New Order - Bid Order
+
+    function buyToken(string symbolName, uint priceInWei, uint amount) {
+
+    }
+
+    // New Order - Ask Order
+
+    function sellToken(string symbolName, uint priceInWei, uint amount) {
+
+    }
+
+    // Cancel Limit Order Logic
+
+    function cancelOrder(string symbolName, bool isSellOrder, uint priceInWei, uint offerKey) {
+
+    }
+
 }
