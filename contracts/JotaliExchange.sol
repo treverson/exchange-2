e807c4c8ae1b4077a1e081f2c06dfd13be744888
diop <fodesdiop@gmail.com>
2018-03-06 11:52:13 -0800

    Implement addToken

--- contracts/JotaliExchange.sol
+++ contracts/JotaliExchange.sol
@@ -47,13 +47,11 @@ contract JotaliExchange is owned {
     uint symbolNameIndex;
 
     // Balances
-
     mapping (address => mapping (uint8 => uint)) tokenBalanceForAddress;
 
     mapping (address => uint) balanceEthForAddress;
 
     // Ether Deposit and Withdrawal
-
     function depositEther() payable {
 
     }
@@ -67,45 +65,64 @@ contract JotaliExchange is owned {
     }
 
     // Token Management
-
     function addToken(string symbolName, address erc20TokenAddress) onlyOwner {
-
+      require(!hasToken(symbolName));
+      symbolNameIndex++;
+      tokens[symbolNameIndex].symbolName = symbolName;
+      tokens[symbolNameIndex].tokenContract = erc20TokenAddress;
     }
 
     function hasToken(string symbolName) constant returns (bool) {
-
+      uint8 index = getSymbolIndex(symbolName);
+      if (index == 0) {
+        return false;
+      }
+      return true
     }
 
     function getSymbolIndex(string symbolName) internal returns (uint8) {
+      for (uint8 i = 1; i <= symbolNameIndex; i++) {
+        if (stringsEqual(tokens[i].symbolName, symbolName)) {
+          return i;
+        }
+      }
+      return 0;
+    }
 
+    // String Comparison Function
+    function stringsEqual(string storage _a, string memory _b) internal returns (bool) {
+      bytes storage a = bytes(_a)
+      bytes memory b = bytes(_b)
+      if (a.length != b.length)
+        return false
+      // @todo unroll the loop
+      for (uint i = 0; i < a.length; i ++)
+        if (a[i] != b[i])
+          return false;
+        return true;
     }
 
     // Order Book - Bid Orders
-
     function getBuyOrderBook(string symbolName) constant returns (uint[], uint[]) {
 
     }
 
     // Order Book - Ask Orders
-
     function getSellOrderBook(string symbolName) constant returns (uint[], uint[]) {
 
     }
 
     // New Order - Bid Order
-
     function buyToken(string symbolName, uint priceInWei, uint amount) {
 
     }
 
     // New Order - Ask Order
-
     function sellToken(string symbolName, uint priceInWei, uint amount) {
 
     }
 
     // Cancel Limit Order Logic
-
     function cancelOrder(string symbolName, bool isSellOrder, uint priceInWei, uint offerKey) {
 
     }
