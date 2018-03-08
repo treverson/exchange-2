4fa9ccab9a2f38c59f3d49691402ceef8abb04fc
diop <fodesdiop@gmail.com>
2018-03-07 23:21:06 -0800

    Pass owner balance test

---
+++ test/JotaliToken.test.js
@@ -0,0 +1,24 @@
+const expect = require('chai').expect
+const JotaliToken = artifacts.require('./JotaliToken')
+const { assertRevert } = require('./utils')
+
+let totalSupply = null
+let owner = null
+let deployed = null
+let account_one = null
+let account_two = null
+let balanceOwner = null
+
+contract('JotaliToken', (accounts) => {
+
+  beforeEach(async () => {
+    deployed = await JotaliToken.deployed()
+  })
+
+  it('owner should own all the supply of tokens', async () => {
+    totalSupply = (await deployed.totalSupply.call()).toNumber()
+    owner = accounts[0]
+    balanceOwner = (await deployed.balanceOf(owner)).toNumber()
+    assert.equal(balanceOwner, totalSupply, 'Total amount of tokens is owned by owner')
+  })
+})
